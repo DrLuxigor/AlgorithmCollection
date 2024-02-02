e53b3c9f11c263bd79f78067dc0cd50392bbd9f0
Lukas Kofler <lukaskofler@hotmail.de>
2024-02-02 23:11:53 +0100

    kruskal mst

--- python/src/graph_tests.py
+++ python/src/graph_tests.py
@@ -1,15 +1,26 @@
 from graphs.graphs import *
 
-n_nodes = 3
+n_nodes = 5
 graph = Graph()
 for i in range(n_nodes):
     graph.nodes.append(Node(node_id=i+1, node_name=f'Node_{i+1}'))
 
-graph.edges.append(Edge(1, 2, 1))
-graph.edges.append(Edge(1, 3, 1))
-graph.edges.append(Edge(1, 1, 1))
+graph.edges.append(Edge(1, 2, 3))
+graph.edges.append(Edge(2, 1, 3))
 
-n1_neighbors = graph.neighbors(graph.nodes[0], return_ids=True)
-n2_neighbors = graph.neighbors(graph.nodes[1], return_ids=True)
-print(n1_neighbors)
-print(n2_neighbors)
+graph.edges.append(Edge(2, 3, 3))
+graph.edges.append(Edge(3, 2, 3))
+
+graph.edges.append(Edge(3, 4, 3))
+graph.edges.append(Edge(4, 3, 3))
+
+graph.edges.append(Edge(4, 5, 3))
+graph.edges.append(Edge(5, 4, 3))
+
+graph.edges.append(Edge(2, 4, 4))
+graph.edges.append(Edge(4, 2, 4))
+
+mst = graph.kruskal_mst()
+
+for edge in mst:
+    print(f"{edge.source} - {edge.dest}: {edge.cost}")
--- python/src/graphs/graphs.py
+++ python/src/graphs/graphs.py
@@ -9,8 +9,43 @@ class Graph:
             return ids
         return [n for n in self.nodes if n.id in ids]
 
-    def min_spanning_tree(self):
-        return
+    def find_parent(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find_parent(parent, parent[i])
+
+    def union(self, parent, rank, x, y):
+        root_x = self.find_parent(parent, x)
+        root_y = self.find_parent(parent, y)
+
+        if rank[root_x] < rank[root_y]:
+            parent[root_x] = root_y
+        elif rank[root_x] > rank[root_y]:
+            parent[root_y] = root_x
+        else:
+            parent[root_y] = root_x
+            rank[root_x] += 1
+
+    def kruskal_mst(self):
+        result = []
+        self.edges = sorted(self.edges, key=lambda edge: edge.cost)
+
+        parent = {}
+        rank = {}
+
+        for node in self.nodes:
+            parent[node.id] = node.id
+            rank[node.id] = 0
+
+        for edge in self.edges:
+            source_root = self.find_parent(parent, edge.source)
+            dest_root = self.find_parent(parent, edge.dest)
+
+            if source_root != dest_root:
+                result.append(edge)
+                self.union(parent, rank, source_root, dest_root)
+
+        return result
 
 
 class Node:
